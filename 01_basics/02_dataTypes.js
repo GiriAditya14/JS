bc58cfe4d0e89a8ab1532487f63f98a2c333d915
GiriAditya14 <145798891+GiriAditya14@users.noreply.github.com>
2024-09-13 15:30:12 +0000

    datatypes commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+// Documentation : tc39 and mdn
+// ECMA script for Standards
+
+"use strict"   // treat all JS code as newer version
+
+// alert( 3 + 3 )   // we are using nodejs, not browser
+
+console.log( 3 
+    + 
+    3 ) // code readability should be high
+
+console.log("Giri")
+
+let name = "aditya" // string
+let age = 18    // number
+let isLoggeddIn = false // boolean
+let state  // undefined
+let temperature = null  // empty
+
+// Primitive data type
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+// Object
+
+console.log(typeof "aditya")
+console.log(typeof age)
+console.log(typeof null)    // object
+console.log(typeof undefined)   // undefined
